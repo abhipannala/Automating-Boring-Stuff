2c6546000570cec3ae62f612120d1dc0f4ff7d6d
Abhi Pannala <puabhiram.reddy@gmail.com>
2019-10-03 19:46:29 -0500

    added first dictionary code and gitignore file

---
+++ Dictionaries/hello_dict.py
@@ -0,0 +1,18 @@
+# Dictionaries are not ordered so they can't be sliced
+import sys
+
+birthdays = {'Alicia': 'Apr 1', 'Bob': 'Jan 15', 'Carol': 'The beginning of all timeA'}
+
+while True:
+    print("Enter a name: (blank to quit)")
+    name = input()
+    if name == '':
+        sys.exit()  # or break, either work
+    elif name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else :
+        print('The person by the name ' + name + ' does not exist in my database')
+        print('What is their birth-date?')
+        bDay = input()
+        birthdays[name] = bDay
+        print('Birthday database updated. Pinging Skynet to implement Doomsday Protocol')
