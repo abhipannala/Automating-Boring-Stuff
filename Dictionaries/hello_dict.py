c595268ed7e5b28762abaf33cf8abf6e98e6f847
Abhi Pannala <abhiram.pannala@dell.com>
2019-10-09 11:02:26 -0500

    Added Character Count and ticTacToe (unfinished) code

--- Dictionaries/hello_dict.py
+++ Dictionaries/hello_dict.py
@@ -1,7 +1,7 @@
 # Dictionaries are not ordered so they can't be sliced
 import sys
 
-birthdays = {'Alicia': 'Apr 1', 'Bob': 'Jan 15', 'Carol': 'The beginning of all timeA'}
+birthdays = {'Alicia': 'Apr 1', 'Bob': 'Jan 15', 'Carol': 'The beginning of all time!'}
 
 while True:
     print("Enter a name: (blank to quit)")
@@ -10,7 +10,7 @@ while True:
         sys.exit()  # or break, either work
     elif name in birthdays:
         print(birthdays[name] + ' is the birthday of ' + name)
-    else :
+    else:
         print('The person by the name ' + name + ' does not exist in my database')
         print('What is their birth-date?')
         bDay = input()
